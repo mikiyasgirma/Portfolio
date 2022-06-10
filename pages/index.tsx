aa69d15c99f2e42069b9c5f3fcd77a5978becc20
mikiyas girma <mikiyasgirma43@gmail.com>
2022-06-10 13:34:35 +0300

    landing page done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,76 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="min-h-screen md:flex md:flex-col justify-center py-6 px-16 bg-vscode-background text-white font-Roboto tracking-wide">
+      <div className="flex flex-col md:flex-row justify-between items-center space-y-12">
+        <div
+          className="bg-white flex flex-col justify-center px-6 py-4 text-gray-400 w-80 h-64 shadow-2xl bg-clip-padding bg-opacity-30 rounded-md "
+          style={{ backdropFilter: "blur(5px)" }}
         >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+          <p className="uppercase text-2xl">
+            This is my world, let's enjoy it together. Rentless Effort.
+          </p>
+          <div className="fixed bottom-4 left-6">mikiyasgirma43@gmail.com</div>
+        </div>
+        <div className="flex flex-col items-center space-y-6">
+          <div className="w-36 h-36 relative">
+            <Image
+              alt="mikiyasgirma"
+              className="rounded-full"
+              src="/assets/me.JPG"
+              layout="fill"
+              objectFit="cover"
+            ></Image>
+          </div>
+          <p className="text-xl">Mikiyas Girma</p>
+          <p className="max-w-xs ">
+            Software Engineer | Web Developer | Full Stack
+          </p>
+          <div className="flex space-x-6">
+            <img src="/assets/github.svg" />
+            <img src="/assets/linkdin.svg" />
+            <img src="/assets/twitter.svg" />
+            <img src="/assets/gmail.svg" />
+          </div>
+        </div>
+        <div className="flex flex-col  space-y-4 ">
+          <div className="text-black font-semibold bg-white w-32 rounded-md px-4 py-1">
+            Projects
+          </div>
 
-export default Home
+          <div className="flex space-x-6 pt-4">
+            <img src="/assets/cart.svg" />
+            <div className="bg-white rounded-md px-4 py-1">
+              <p className="text-black font-semibold">Ecommerce | Turmii</p>
+            </div>
+          </div>
+          <div className="flex space-x-6 pt-4">
+            <img src="/assets/ERP.svg" />
+            <div className="bg-white rounded-md px-4 py-1">
+              <p className="text-black font-semibold">ERP | Tekron</p>
+            </div>
+          </div>
+          <div className="flex space-x-6 pt-4">
+            <img src="/assets/inventory.svg" />
+            <div className="bg-white rounded-md px-4 py-1">
+              <p className="text-black font-semibold">Inventory | Suk</p>
+            </div>
+          </div>
+          <div className="flex space-x-6 pt-4">
+            <img src="/assets/business.svg" />
+            <div className="bg-white rounded-md px-4 py-1">
+              <p className="text-black font-semibold">
+                Business | Akiya Coffee
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "vscode-background": "url('/assets/vs-code-back.jpg')",
+      }),
+      fontFamily: {
+        Roboto: ["Roboto mono", "monospace"],
+      },
+    },
   },
   plugins: [],
-}
+};
